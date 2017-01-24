9938e0a25af7da7f27ee959d16449f01ec978254
storybasin <storybasin88@naver.com>
2017-01-24 14:47:01 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ public class Hello{
 		System.out.println("what is Git?");
 		System.out.println("That's DVCS")
 		System.out.println("2002 BitKeeper DVCS");
-
+		System.out.println("That's version Control");
 
 	}
 }
